9bef8c61bd652ff111bdbea02f7a1be83e3ae0d5
toonneedmoneyyyy <punnapa.tri@dome.tu.ac.th>
2024-09-22 22:06:08 +0700

    2test

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+
 if ! which node &> /dev/null; then
   echo "Installing Node.js..."
   curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
   sudo install nodejs -y
+
 fi
 
-
+if ! which yarn &> /dev/null; then
+  echo "Installing yarn..."
+  npm install -g yarn
+  yarn add react-scripts
+fi
 
 if ! which pm2 &> /dev/null; then
   echo "Installing pm2..."
   npm install -g pm2
 fi
 
-cd testsprint1 
 
-npm install
+#cd pet-adoption
 
-npm start
+yarn install
+
+yarn start
